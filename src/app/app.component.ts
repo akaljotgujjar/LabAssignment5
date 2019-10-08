fd4c762e9530b864331931320729e2fafd2c09c1
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-10-07 19:27:12 -0700

    final product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LabAssignment3';
+
+  // firstName: string;
+  // lastName: string;
+  // private firstName: string;
+  // private lastName: string;
+
+  // constructor() {
+  //   this.firstName = 'Akaljot';
+  //   this.lastName = 'Gujjar';
+  // }
+
+  // showGreeting() {
+  //   const row2 = document.getElementById('r1c2');
+  //   row2.innerHTML = 'Hello ' + this.firstName + ' ' + this.lastName + '.';
+  // }
+
+  // resetGreeting() {
+  //   const row2 = document.getElementById('r1c2');
+  //   row2.innerHTML = 'r1c2';
+  // }
 }
